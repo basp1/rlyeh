57b5fe333bf40d8a93cb966d66f1540099d28411
basp <akovn@yandex.ru>
2019-11-12 14:07:53 +0300

    Add Animation

---
+++ animation.go
@@ -0,0 +1,41 @@
+package rlyeh
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type Animation struct {
+	Texture rl.Texture2D
+
+	point rl.Vector2
+	size  Size
+
+	frameCount byte
+}
+
+func New(texture rl.Texture2D, point rl.Vector2, size Size, frameCount byte) *Animation {
+	self := &Animation{}
+
+	self.point = point
+	self.size = size
+	self.frameCount = frameCount
+
+	self.Texture = texture
+
+	return self
+}
+
+func (self *Animation) Get(frame byte) rl.Rectangle {
+	current := frame
+
+	if current >= self.frameCount {
+		current = current % self.frameCount
+	}
+
+	return rl.Rectangle{
+		X:      self.point.X + float32(current)*self.size.Width,
+		Y:      self.point.Y,
+		Width:  self.size.Width,
+		Height: self.size.Height,
+	}
+}
---
+++ animation_test.go
@@ -0,0 +1,32 @@
+package rlyeh
+
+import (
+	"testing"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAnimation(T *testing.T) {
+	a := New(rl.Texture2D{}, rl.Vector2{X: 0, Y: 0}, Size{Width: 32, Height: 32}, 2)
+
+	frame := a.Get(0)
+
+	assert.Equal(T, float32(0), frame.X)
+	assert.Equal(T, float32(0), frame.Y)
+
+	frame = a.Get(1)
+
+	assert.Equal(T, float32(32), frame.X)
+	assert.Equal(T, float32(0), frame.Y)
+
+	frame = a.Get(2)
+
+	assert.Equal(T, float32(0), frame.X)
+	assert.Equal(T, float32(0), frame.Y)
+
+	frame = a.Get(3)
+
+	assert.Equal(T, float32(32), frame.X)
+	assert.Equal(T, float32(0), frame.Y)
+}
